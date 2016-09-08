3ef9860cd9cedbd536475d1174014d7a93e47d5c
Vithoh <V.Grzonk@gmx.de>
2016-09-08 21:11:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/*
+##########################################################################
+#                                                                        #
+#   Copyright 2016 by Ignas Kavaliauskas & Viktor Grzonkowski            #
+#                                                                        #
+##########################################################################
+*/
+
+// important data include
+include("_mysql.php");
+
+// end important data include
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01Transitional
+//EN""http://www.w3.org/TR/html4/loose.dtd">
+<html lang="de">
+		<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<meta name="keywords" content="" />
+			<meta name="copyright" content="Copyright &copy; 2016 by Ignas Kavaliauskas & Viktor Grzonkowski" />
+
+			<!-- Head & Title include -->
+			<title><?php echo "KIM Board"; ?></title>
+			<link href="_stylesheet.css" rel="stylesheet" type="text/css" />
+			<!-- end Head & Title include -->
+			
+		</head>
+		
+		
+		
+		
+		<body>
+		<img src="images/bg.jpg" style="position:absolute; left:0; top:0; width:100%; height:100%; z-index:0;" />
+		<img src="images/rahmen.png" style="position:absolute; left:0; top:0; width:100%; height:100%; z-index:0;" />
+		
+		
+		</body>
+</html>
