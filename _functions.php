15965759fb432fca5146ba0dc719af7c7538b103
IGnoXX <ignoxx@yahoo.de>
2016-09-08 22:14:09 +0200

    replaced files

--- _functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function foo ($arg_1, $arg_2, ..., $arg_n)
-{
-    echo "Beispielfunktion.\n";
-    return $retval;
-}
-?>
--- _mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-	 $host = "localhost";
-	 $user = "root@localhost";
-	 $pwd = "";
-	 $db = "test";
-	 //define("PREFIX", 'ws_DyN_');
-	?>
