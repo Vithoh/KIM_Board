0d110c86073a8aacfdae5a321bf3c321e788d02e
Vithoh <V.Grzonk@gmx.de>
2016-09-12 00:13:37 +0200

    Revert "db file"
    This reverts commit ceff8f484e33ba58962d430b757fc0752d11f6d9.

--- LOGIN  REGISTER/!!!!DATENBANK!!!!.sql
+++ LOGIN  REGISTER/!!!!DATENBANK!!!!.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `users` (
 `uid` int NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+`username` varchar(25) NOT NULL UNIQUE,
 `password` varchar(200) NOT NULL ,
 `email` varchar(100) NOT NULL,
+`name` varchar(100) NOT NULL,
 `profile_pic` varchar(200) NOT NULL
 );
