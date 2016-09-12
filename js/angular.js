b18ad884fd2e62c8e9502f41a77098a38d7e80cb
IGnoXX <ignoxx@yahoo.de>
2016-09-12 21:32:45 +0200

    routing

--- js/angular.js
+++ js/angular.js
@@ -1,24 +1,44 @@
 /**
      * You must include the dependency on 'ngMaterial' 
      */
-    var app = angular.module('Kim_Board', ['ngMaterial',"ngRoute"]);
+    var app = angular.module('Kim_Board', ['ngMaterial',"ngRoute", 'ui.router']);
 
-app.config(function($routeProvider) {
-  $routeProvider
-  .when("/", {
-    url : "/"
-  })
-  .when("/login", {
-    templateUrl : "template/login.htm"
-  })
-  .when("/register", {
-    templateUrl : "template/register.htm"
-  })
+/*app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        template : "<h1>Main</h1><p>Click on the links to change this content</p>"
+    })
+    .when("/login", {
+        template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
+    })
+    .when("/register", {
+        template : "<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>"
+    });
 });
+*/
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+ 
+    $stateProvider
+        .state('login', {
+            url:'/login',
+            template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
+        })
 
-app.controller("menuCtrl", ["$scope", function($scope){
-    $scope.test = function(){
-        alert("penisss");
+        .state('register', {
+            url:'/register',
+            template : "<h1>asdasds</h1><p>Bananas contain around 75% water.</p>"
+        })
+ 
+}]);
+
+app.controller("menuCtrl", ["$scope", "$state", function($scope, $state){
+    $scope.login = function(){
+        $state.go('login');
+    }
+
+    $scope.register = function(){
+        $state.go('register')
     }
 
 }])
